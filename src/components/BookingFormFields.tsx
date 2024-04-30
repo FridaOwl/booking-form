1b9eb0fdcbbb199d1aa0f2d26648ced131b23f84
FridaOwl <farvin.lori@gmail.com>
2024-04-30 13:48:24 +0300

    upd:border

--- src/components/BookingFormFields.tsx
+++ src/components/BookingFormFields.tsx
@@ -4,12 +4,9 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './BookingFormFields.css';
 
-
-
 interface BookingFormFieldsProps {
     onSubmit: (values: any) => void;
 }
-
 
 const BookingFormFields: React.FC<BookingFormFieldsProps> = ({ onSubmit }) => {
     const handleSubmit = (values: any) => {
@@ -20,79 +17,70 @@ const BookingFormFields: React.FC<BookingFormFieldsProps> = ({ onSubmit }) => {
         <Form
             onSubmit={handleSubmit}
             render={({ handleSubmit }) => (
-                <form onSubmit={handleSubmit}>
-                    <div>
+                <form onSubmit={handleSubmit} className="formContainer">
+
+                    <div className="formGroup">
                         <label>Дата:</label>
                         <Field name="date">
                             {({ input }) => (
-                                <DatePicker {...input} selected={input.value} dateFormat="dd/MM/yyyy" />
+                                <DatePicker
+                                    {...input}
+                                    selected={input.value}
+                                    dateFormat="dd/MM/yyyy"
+                                    className="customInput"
+                                />
                             )}
                         </Field>
-                    </div>
-
-                    <div>
                         <label>Время:</label>
-                        <Field name="time">
-                            {({ input }) => (
-                                <select {...input}>
-                                    <option value="">Время</option>
-                                    <option value="09:00">09:00</option>
-                                    <option value="10:00">10:00</option>
-                                    <option value="11:00">11:00</option>
-                                    <option value="12:00">12:00</option>
-                                    <option value="13:00">13:00</option>
-                                    <option value="14:00">14:00</option>
-                                    <option value="15:00">15:00</option>
-                                </select>
-                            )}
+                        <Field name="time" component="select" className="customInput">
+                            <option value="">Выберите время</option>
+                            <option value="09:00">09:00</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                            <option value="13:00">13:00</option>
+                            <option value="14:00">14:00</option>
+                            <option value="15:00">15:00</option>
                         </Field>
-                    </div>
-
-                    <div>
                         <label>Услуга:</label>
-                        <Field name="service" component="select">
-                                    <option value="">Выберите услугу</option>
-                                    <option value="Массаж">Массаж</option>
-                                    <option value="Парикмахерская услуга">Парикмахерская услуга</option>
-                                    <option value="Косметология">Косметология</option>
-                                    <option value="Ногтевой сервис">Ногтевой сервис</option>
-                        </Field>
-                    </div>
-
-
-                    <div>
+                        <Field name="service" component="select" className="customInput">
+                            <option value="">Выберите услугу</option>
+                            <option value="Массаж">Массаж</option>
+                            <option value="Парикмахерская услуга">Парикмахерская услуга</option>
+                            <option value="Косметология">Косметология</option>
+                            <option value="Ногтевой сервис">Ногтевой сервис</option>
+                            </Field>
+                        
                         <label>Филиал:</label>
-                        <Field name="branch" component="select">
-                                    <option value="">Выберите филиал</option>
-                                    <option value="Филиал 1">Филиал 1</option>
-                                    <option value="Филиал 2">Филиал 2</option>
-                                    <option value="Филиал 3">Филиал 3</option>
-                                    <option value="Филиал 4">Филиал 4</option>
+                        <Field name="branch" component="select" className="customInput">
+                            <option value="">Выберите филиал</option>
+                            <option value="Филиал 1">Филиал 1</option>
+                            <option value="Филиал 2">Филиал 2</option>
+                            <option value="Филиал 3">Филиал 3</option>
+                            <option value="Филиал 4">Филиал 4</option>
                         </Field>
                     </div>
 
 
-                    <div>
+                    <div className="formGroup">
                         <label>Имя:</label>
-                        <Field name="firstName" component="input" type="text" />
-                    </div>
-
-                    <div>
+                        <Field name="firstName" component="input" type="text" className="customInput" />
                         <label>Фамилия:</label>
-                        <Field name="lastName" component="input" type="text" />
+                        <Field name="lastName" component="input" type="text" className="customInput" />
                     </div>
 
-                    <div>
+
+                    <div className="formGroup">
                         <label>Номер телефона:</label>
-                        <Field name="phoneNumber" component="input" type="tel" />
-                    </div>
-
-                    <div>
+                        <Field name="phoneNumber" component="input" type="tel" className="customInput" />
                         <label>Комментарий:</label>
-                        <Field name="comment" component="textarea" />
+                        <Field name="comment" component="textarea" className="customInput" />
                     </div>
 
-                    <button type="submit">Заказать</button>
+
+                    <div className="formGroup">
+                        <button type="submit" className="submitButton">Заказать</button>
+                    </div>
                 </form>
             )}
         />
